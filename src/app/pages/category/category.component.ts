b1fa80cdc57bcaba9ea2f267f71912ee7efc3dbf
diego caviedes <diego.caviedes@cargologik.com>
2022-10-15 00:26:59 -0500

    pagina de categoria

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  categoryId: string | null = null;
+  limit = 10;
+  offset = 0;
+  products: Product[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService
+    ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.categoryId = params['id'];
+      if(this.categoryId){ // se coloca este if para que no este nulla y no arroje error
+        this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
+        .subscribe(data => {
+          this.products = data;
+        })
+      }
+    })
   }
 
 }
