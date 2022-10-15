f5d6e48a610eef1948a03b7088f204049f24ecbc
diego caviedes <diego.caviedes@cargologik.com>
2022-10-15 15:12:04 -0500

    implementado routerLink y routerActive

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { StoreService } from '../../services/store.service'
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../models/user.model';
+import { CategoriesService } from 'src/app/services/categories.service';
+import { Category } from 'src/app/models/category.model';
 
 @Component({
   selector: 'app-nav',
@@ -14,16 +16,19 @@ export class NavComponent implements OnInit {
   activeMenu = false;
   counter = 0;
   profile: User | null = null;
+  categories: Category[] = [] 
 
   constructor(
     private storeService: StoreService,
     private authService: AuthService,
+    private categoriesService: CategoriesService
   ) { }
 
   ngOnInit(): void {
     this.storeService.myCart$.subscribe(products => {
       this.counter = products.length;
     });
+    this.getAllCategories();
   }
 
   toggleMenu() {
@@ -35,6 +40,14 @@ export class NavComponent implements OnInit {
     .subscribe(user => {
       this.profile = user;
     });
+  }
+
+  getAllCategories() {
+    this.categoriesService.getAll()
+    .subscribe(categories => {
+      console.log(categories)
+        this.categories = categories;
+      });
   }
 
 }
