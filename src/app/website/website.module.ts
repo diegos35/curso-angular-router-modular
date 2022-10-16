1834e64df44511aad63ebdc88d9e70be28330e41
diego caviedes <diego.caviedes@cargologik.com>
2022-10-16 02:07:04 -0500

    creando en website Module, creando dos modulos cms y website

---
+++ src/app/website/website.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ImgComponent } from './components/img/img.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ReversePipe } from './pipes/reverse.pipe';
+import { TimeAgoPipe } from './pipes/time-ago.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
+
+import { HomeComponent } from './pages/home/home.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { MycartComponent } from './pages/mycart/mycart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+
+
+import { WebsiteRoutingModule } from './website-routing.module';
+import { LayoutComponent } from './components/layout/layout.component';
+import { SwiperModule } from 'swiper/angular';
+
+
+@NgModule({
+  declarations: [
+    ImgComponent,
+    ProductComponent,
+    ProductsComponent,
+    NavComponent,
+    ReversePipe,
+    TimeAgoPipe,
+    HighlightDirective,
+    HomeComponent,
+    CategoryComponent,
+    MycartComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecoveryComponent,
+    ProfileComponent,
+    ProductDetailComponent,
+    LayoutComponent
+  ],
+  imports: [
+    CommonModule,
+    WebsiteRoutingModule,
+    SwiperModule
+  ]
+})
+export class WebsiteModule { }
